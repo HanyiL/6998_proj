10bb23e8292792816fb841f27a239db9215bf703
HanyiL <16hl45@queensu.ca>
2021-12-15 13:20:11 -0500

    cnn arcs

--- generateCaption.py
+++ generateCaption.py
@@ -9,7 +9,6 @@ from build_vocab import Vocabulary
 from model import EncoderCNN, DecoderRNN
 from PIL import Image
 
-
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -34,7 +33,7 @@ def main(args):
         vocab = pickle.load(f)
 
     # Build models
-    encoder = EncoderCNN(args.embed_size).eval()  # eval mode (batchnorm uses moving mean/variance)
+    encoder = EncoderCNN(args.embed_size,args.cnn_arc).eval()  # eval mode (batchnorm uses moving mean/variance)
     decoder = DecoderRNN(args.embed_size, args.hidden_size, len(vocab), args.num_layers)
     encoder = encoder.to(device)
     decoder = decoder.to(device)
@@ -68,9 +67,10 @@ def main(args):
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--cnn_arc', type=str , default='res18', help='choose different CNN architecture for training')
     parser.add_argument('--image', type=str, required=True, help='input image for generating caption')
-    parser.add_argument('--encoder_path', type=str, default='models/encoder-5-3000.pkl', help='path for trained encoder')
-    parser.add_argument('--decoder_path', type=str, default='models/decoder-5-3000.pkl', help='path for trained decoder')
+    parser.add_argument('--encoder_path', type=str, default='models_res18/encoder-5-3000.ckpt', help='path for trained encoder')
+    parser.add_argument('--decoder_path', type=str, default='models_res18/decoder-5-3000.ckpt', help='path for trained decoder')
     parser.add_argument('--vocab_path', type=str, default='data/vocab.pkl', help='path for vocabulary wrapper')
     
     # Model parameters (should be same as paramters in train.py)
--- model.py
+++ model.py
@@ -3,34 +3,46 @@ import torch.nn as nn
 import torchvision.models as models
 from torch.nn.utils.rnn import pack_padded_sequence
 
-
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size, CNNModel):
-        """Load the pretrained ResNet-152 and replace top fc layer."""
         super(EncoderCNN, self).__init__()
-        if CNNModel == 'res18':
-            resnet = models.resnet18(pretrained=True)
-        elif CNNModel == 'res34':
-            resnet = models.resnet34(pretrained=True)
-        elif CNNModel == 'res50':
-            resnet = models.resnet50(pretrained=True)
-        elif CNNModel == 'res101':
-            resnet = models.resnet101(pretrained=True)
-        elif CNNModel == 'res152':
-            resnet = models.resnet152(pretrained=True)
-        modules = list(resnet.children())[:-1]      # delete the last fc layer.
-        self.resnet = nn.Sequential(*modules)
-        self.linear = nn.Linear(resnet.fc.in_features, embed_size)
-        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
-        
+        if 'res' in CNNModel:
+          if CNNModel == 'res18':
+              cnnNet = models.resnet18(pretrained=True)
+          elif CNNModel == 'res34':
+              cnnNet = models.resnet34(pretrained=True)
+          elif CNNModel == 'res50':
+              cnnNet = models.resnet50(pretrained=True)
+          elif CNNModel == 'res101':
+              cnnNet = models.resnet101(pretrained=True)
+          elif CNNModel == 'res152':
+              cnnNet = models.resnet152(pretrained=True)
+          modules = list(cnnNet.children())[:-1]      # delete the last fc layer.
+          self.cnnNet = nn.Sequential(*modules)
+          self.linear = nn.Linear(cnnNet.fc.in_features, embed_size)
+          self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+        else:
+          if CNNModel == 'vgg11':
+            self.cnnNet = models.vgg11(pretrained=True)
+          elif CNNModel == 'vgg13':
+            self.cnnNet = models.vgg13(pretrained=True)
+          elif CNNModel == 'vgg16':
+            self.cnnNet = models.vgg16(pretrained=True)
+          elif CNNModel == 'vgg19':
+            self.cnnNet = models.vgg19(pretrained=True)
+          modules = list(self.cnnNet.classifier.children())[:-1]
+          self.cnnNet.classifier = nn.Sequential(*modules)
+          self.linear = nn.Linear(in_features=4096, out_features=embed_size)
+          self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+
+
     def forward(self, images):
         """Extract feature vectors from input images."""
         with torch.no_grad():
-            features = self.resnet(images)
+            features = self.cnnNet(images)
         features = features.reshape(features.size(0), -1)
         features = self.bn(self.linear(features))
         return features
-
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers, max_seq_length=20):
